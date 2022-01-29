9a237d9c55416593d92ad8deeb183f6265cb850b
CodeBXTechnologies <98498111+CodeBXTechnologies@users.noreply.github.com>
2022-01-29 02:51:21 +0200

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,83 @@
+
+
+print("hellow world . this is my first python code")
+
+li = ['ast', 6, "tinashe", "kundai", 45]
+print(li)
+li = li.pop()
+
+# LECTURE 1
+"""
+STARTING LEARNING PYTHON
+import keyword
+print(keyword.kwlist)
+a = 10; b = 20; c = b + a
+print(a); print(b) ; print(c)
+"""
+"""
+# STRINGS IN PYTHON
+# Assigning string to a variable
+g = "This is a string"
+print(g)
+"""
+
+# LECTURE 2
+
+
+
+# DECLARING A LIST
+L = [1, "a", "string", 1+2]
+print(L)
+L.append(6)
+print(L)
+L.pop()
+print(L)
+print(L[3])
+
+
+# LECTURE 3
+
+"""
+TUPLES IN PYTHON 
+Tuples in Python 
+A tuple is a sequence of immutable Python objects. 
+Tuples are just like lists with the exception that tuples cannot be changed once declared. 
+Tuples are usually faster than lists.
+
+"""
+"""
+tup = (1, "a", "string", 1+2)
+print(tup)
+print(tup[3])
+"""
+
+# PYTHON ITERATION
+#WHILE LOOP
+"""
+i = 1
+while (i< 10) :
+    print(i)
+    i += 1
+"""
+"""
+# Example 2: Iteration by for loop on string
+s = "Hello World"
+for i in s:
+    print(i)
+"""
+# Example 3: Iteration by for loop on list
+
+L = [1, 4, 5, 7, 8, 9]
+for i in L:
+    print(i)
+
+# Example 4 : Iteration by for loop for range
+for i in range(0, 10):
+    print(i)
+
+# Python String
+"""
+In Python, Strings are arrays of bytes representing Unicode characters. 
+However, Python does not have a character data type, a single character is simply a string with a length of 1. 
+Square brackets can be used to access elements of the string.
+"""
